de54c03f6ff6c2f86007fc1e5d815bf8800bf84d
DayanaK11 <dayanacayol@gmail.com>
2024-04-27 13:50:04 -0500

    model security y repository

--- src/main/java/pe/edu/cibertec/WAEC2CayoLahuanaDayanaKatherine/model/db/Rol.java
+++ src/main/java/pe/edu/cibertec/WAEC2CayoLahuanaDayanaKatherine/model/db/Rol.java
@@ -1,4 +1,16 @@
 package pe.edu.cibertec.WAEC2CayoLahuanaDayanaKatherine.model.db;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "rol")
 public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idrol;
+    private String nomrol;
 }
---
+++ src/main/java/pe/edu/cibertec/WAEC2CayoLahuanaDayanaKatherine/repository/RolRepository.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.WAEC2CayoLahuanaDayanaKatherine.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.cibertec.WAEC2CayoLahuanaDayanaKatherine.model.db.Rol;
+
+@Repository
+public interface RolRepository extends
+        JpaRepository<Rol, Integer> {
+
+    Rol findByNomrol(String nombrerol);
+
+}
+
